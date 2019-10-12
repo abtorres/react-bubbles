bfe741bf33c6b521bef9806830f58d18d5086306
abtorres <abtorres079@gmail.com>
2019-10-12 13:36:53 -0600

    typo

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -19,7 +19,7 @@ const ColorList = ({ colors, updateColors }) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is it saved right now?
     let activeColor = colors.filter(color => color.id === colorToEdit.id);
 
     axiosWithAuth()
@@ -34,6 +34,7 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    
     axiosWithAuth()
       .delete(`http://localhost:5000/api/colors/${color.id}`)
       .then(res => console.log(res))
